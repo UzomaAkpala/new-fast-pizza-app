b1501d2a12ba26a5a582a72d26276eb4229edd73
UzomaAkpala <francisakpala@yahoo.com>
2023-10-04 12:30:42 +0100

    Box model: Spacing, Border and displays

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Uzoma</div>;
+  return <div className="hidden text-sm font-semibold">Uzoma</div>;
 }
 
 export default Username;
